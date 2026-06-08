00a0e1084db3d8cc546629f2073aac94d0afe048
jeongseok <wjdtjr001226@naver.com>
2026-06-08 22:12:02 +0900

    init: create mini-order-service project

---
+++ src/main/java/com/sparta/miniorderservice/MiniOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.miniorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniOrderServiceApplication.class, args);
+    }
+
+}
